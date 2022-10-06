ceb5cd3f01d513010bfb16e34fffc7a72e10e1b9
linKhehe <35095843+linKhehe@users.noreply.github.com>
2022-10-06 12:08:40 -0500

    publish

--- a2squery/__init__.py
+++ a2squery/__init__.py
@@ -6,7 +6,7 @@ __title__ = "a2squery"
 __author__ = "Liam (linKhehe) Henderson"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022-present Liam (linKhehe) Henderson"
-__version__ = "0.0.0"
+__version__ = "0.0.2"
 
 __all__ = (
     "A2SQuery", "SourceInfo", "GoldSourceInfo",
