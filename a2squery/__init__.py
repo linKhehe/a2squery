05db5672fe6cc046035ca642126738f0fda4ed19
linKhehe <35095843+linKhehe@users.noreply.github.com>
2022-10-06 11:41:59 -0500

    publish

--- a2squery/__init__.py
+++ a2squery/__init__.py
@@ -2,6 +2,12 @@ from .query import A2SQuery
 from .data import SourceInfo, GoldSourceInfo, Player
 from .enums import ServerType, Environment
 
+__title__ = "a2squery"
+__author__ = "Liam (linKhehe) Henderson"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present Liam (linKhehe) Henderson"
+__version__ = "0.0.0"
+
 __all__ = (
     "A2SQuery", "SourceInfo", "GoldSourceInfo",
     "Player", "ServerType", "Environment"
--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
+import re
 from setuptools import setup
+
+
+def read_requirements(filename):
+    try:
+        with open(filename) as f:
+            return f.read().split("\n")
+    except FileNotFoundError:
+        return []
+
+
+with open("a2squery/__init__.py") as f:
+    version = re.search(r"__version__ *= *[\"'](.*)[\"']", f.read()).group(1)
 
 with open("README.md") as f:
     readme = f.read()
+
+requirements = read_requirements("requirements.txt")
+extras_require = {
+    "docs": read_requirements("docs/requirements.txt")
+}
 
 setup(name="a2squery",
       author="linKhehe",
@@ -10,15 +28,16 @@ setup(name="a2squery",
         "Documentation": "https://a2squery.readthedocs.io/en/latest/",
         "Issue tracker": "https://github.com/linKhehe/a2squery/issues",
       },
-      version="0.0.1",
+      version=version,
       license="MIT",
       description="A2SQuery is a python implementation of Valve's A2S protocol",
       long_description=readme,
       long_description_content_type="text/x-rst",
+      install_requires=requirements,
+      extras_require=extras_require,
       include_package_data=True,
       python_requires=">=3.6.0",
       classifiers=[
-        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Natural Language :: English",
